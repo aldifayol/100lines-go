6dbba01a273ceead7de1d827e6cdc3590a43172e
Aldi <aldifayol@gmail.com>
2025-08-08 21:25:19 +0800

    feat: modify slice

--- arrays_slices_maps/slices.go
+++ arrays_slices_maps/slices.go
@@ -1,10 +1,23 @@
 package array_slice_maps
 
+import "fmt"
+
 func MySlices(s int) string{
 
-	var mySlice_1 = []string{
-		"got you",
-		"you know what",
+	/* Define an array containing programming languages */
+	languages := [9]string{
+		"C", "Lisp", "C++", "Java", "Python",
+		"JavaScript", "Ruby", "Go", "Rust", // Must include the trailing comma
 	}
-	return mySlice_1[s]
+
+	/* Define slices */
+	classics := languages[0:3]  // alternatively languages[:3]
+	modern := languages[3:7]     // include 3 exclude 7
+	new := languages[7:9]       // alternatively languages[7:]
+
+	fmt.Printf("classic languagues: %v\n", classics) // classic languagues: [C Lisp C++]
+	fmt.Printf("modern languages: %v\n", modern)     // modern languages: [Java Python JavaScript Ruby]
+	fmt.Printf("new languages: %v\n", new) 
+
+	return "string"
 }
